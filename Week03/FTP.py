df46b4da4c0795fb8e0f571fd84b5877e100c3e9
Klenn <johnklenn@gmail.com>
2024-04-15 14:11:22 +0800

    Added upload_to_ftp function

--- Week03/FTP.py
+++ Week03/FTP.py
@@ -19,6 +19,11 @@ def get_ftp() -> FTP_TLS:
     ftp.prot_p()
     return ftp
 
+# Function to upload csv files to the FTP server
+def upload_to_ftp(ftp: FTP_TLS, file_source: Path):
+    with open(file_source, "rb") as fp:
+        ftp.storbinary(f"STOR {file_source.name}", fp)
+
 # Function to read csv files from the URL
 def read_csv(config: dict) -> pd.DataFrame:
     url = config["URL"]
@@ -33,12 +38,12 @@ if __name__=="__main__":
     with open("config.json", "rb") as fp:
         config = json.load(fp)
 
-    # Itteration for each source in the config.json file
+    # Itterating each source in the config.json file
     for source_name, source_config in config.items():
         file_name = source_name + ".csv"
         df = read_csv(source_config)
         
         # Download to local
         df.to_csv(file_name, index=False)
-        print(f'Downloaded {file_name} successfully')
+        print(f'Downloaded {file_name} successfully!')
 
