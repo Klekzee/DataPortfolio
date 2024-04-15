efcc0f6468893492be5e58a83bf85354d07427dd
Klenn <johnklenn@gmail.com>
2024-04-15 14:25:01 +0800

    Added delete_file function and scheduler

--- Week03/FTP.py
+++ Week03/FTP.py
@@ -24,26 +24,45 @@ def upload_to_ftp(ftp: FTP_TLS, file_source: Path):
     with open(file_source, "rb") as fp:
         ftp.storbinary(f"STOR {file_source.name}", fp)
 
+# Function to delete files from local system
+def delete_file(file_source: str | Path):
+    remove(file_source)
+
 # Function to read csv files from the URL
 def read_csv(config: dict) -> pd.DataFrame:
     url = config["URL"]
     params = config["PARAMS"]
     return pd.read_csv(url, **params)
 
-
-
-if __name__=="__main__":
-    
+def pipeline():
     # Load source configuration from config.json file
     with open("config.json", "rb") as fp:
         config = json.load(fp)
 
+    # Get FTP_TLS object
+    ftp = get_ftp()
+
     # Itterating each source in the config.json file
     for source_name, source_config in config.items():
-        file_name = source_name + ".csv"
+        file_name = Path(source_name + ".csv")
         df = read_csv(source_config)
         
         # Download to local
         df.to_csv(file_name, index=False)
         print(f'Downloaded {file_name} successfully!')
 
+        # Upload to FTP server
+        upload_to_ftp(ftp, file_name)
+        print(f'Uploaded {file_name} successfully!')
+
+        # Delete the local file
+        delete_file(file_name)
+        print(f'Deleted {file_name} successfully!')
+
+if __name__=="__main__":
+    
+    schedule.every().day.at('00:00').do(pipeline)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
